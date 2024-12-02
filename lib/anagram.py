8f9c507c42c8d7d32d51acdc41061968903168ad
bettyje <bettyjelagat156@gmail.com>
2024-12-02 14:04:14 +0300

    complete Anagram Detector

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,16 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, word_list):
+        anagrams = []
+
+        sorted_word = sorted(self.word)
+        anagrams = [word for word in word_list if sorted(word) == sorted_word]
+        return anagrams
+
+
+# anagram_checker = Anagram("listen")
+# result = anagram_checker.match(
+#     ["enlist", "google", "silent", "inlets", "hello"])
+# print(result)
